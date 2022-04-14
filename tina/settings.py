0fbcaaabb371433029929da9b9da21e009cdedae
Boudjema Ali <boudjema.ali94@gmail.com>
2022-04-14 22:21:14 +0100

    Remove key

--- tina/settings.py
+++ tina/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r4b@v7o$)zqn3^=!p#-6la$q*7&usadvi_c9)@s_yvj)dt2#!p'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
